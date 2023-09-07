b50add4912b706e2429c870d67f6150e070d1c25
Yash Kumar Sharma <118594034+YashSharma-001@users.noreply.github.com>
2023-09-07 09:35:09 -0700

    Add files via upload

---
+++ assign2/Console_Lab2_Exercise_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Lab2_Exercise_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.display();
+            Console.ReadLine();
+        }
+        public class Employee
+        {
+            int age;
+            string name;
+            double salary;
+            public void display()
+            {
+                name = "Gopal Soni";
+                age = 22;
+                salary = 45000;
+                Console.WriteLine("Name of Employee: " + name + "\n Age of Employee: " + age + "\n Salary of Employee: " + salary);
+            }
+        }
+    }
+}
---
+++ assign2/Console_Lab2_Exercise_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Lab2_Exercise_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.display();
+            Console.ReadLine();
+        }
+        public class BankAccount
+        {
+            double account_number;
+            string account_holdername;
+            double account_balance = 50000;          
+            double addmoney = 80000;
+            double withdrawalmoney = 8000;
+            public void deposit()
+            {
+                account_balance += addmoney;
+            }
+            public void withdrawal()
+            {
+                account_balance -= withdrawalmoney;
+            }
+            public void display()
+            {
+                Console.WriteLine("Add some money to your account: " + addmoney);
+                Console.WriteLine("deducted some money to your account: " + withdrawalmoney);
+                Console.WriteLine("Money after deposit: " + (account_balance + addmoney));
+                Console.WriteLine("Money after withdrawal: " + (account_balance - withdrawalmoney));
+            }
+        }
+    }
+}
